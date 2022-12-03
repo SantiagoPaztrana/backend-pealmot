381452b9a7176b1a426bed434fb05e524012f8e8
Santiago Garzón <dpaztrana.sd@gmail.com>
2022-12-03 17:37:08 -0500

    first commit

---
+++ controllers/productoController.js
@@ -0,0 +1,147 @@
+import Producto from "../models/Producto.js";
+import fs from "fs-extra";
+
+import{
+
+    uploadImage,
+    deleteImage
+} from '../helper/cloudinary.js';
+
+
+const prueba = (req, res)=>{
+
+    res.send({
+
+    msg:"En esta ruta gestionaremos todas las peticiones correspondiente al modelo de Productos"
+    
+    })
+    };
+
+const createProductos = async (req, res) => {
+    try {
+        const { nombre, description, precio, stock } = req.body;
+        let image;
+        console.log(nombre)
+        
+        if (req.files.image) {
+            const result = await uploadImage(req.files.image.tempFilePath);
+            await fs.remove(req.files.image.tempFilePath);
+
+            image = {
+                url: result.secure_url,
+                public_id: result.public_id,
+            };
+            
+            //console.log(result);
+        }
+
+        const Newproducto = new Producto({ nombre, description, precio, image, stock });
+        await Newproducto.save();
+
+        return res.json(Newproducto);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msg: error.message });
+    }
+};
+
+
+const getProductos = async (req, res) => {
+    try {
+        const productos = await Producto.find();
+        res.send(productos);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message });
+
+        }
+    };
+
+const updateProductos = async (req, res) => {
+
+    const { id } = req.params;
+    const { nombre, description, precio, stock } = req.body;
+    //console.log(id, nombre, description, precio, stock);
+    //console.log(req.files.image);
+    try {
+        const updateProducto = await Producto.findById(id);
+
+        updateProducto.nombre = nombre;
+        updateProducto.description = description;
+        updateProducto.precio = precio;
+        updateProducto.stock = stock;
+
+        if(req.files !== null){
+            if (req.files.image) {
+                await deleteImage(updateProducto.image.public_id);
+                const result = await uploadImage(req.files.image.tempFilePath);
+
+                await fs.remove(req.files.image.tempFilePath);
+                
+                updateProducto.image = {
+                    url: result.secure_url,
+                    public_id: result.public_id,
+                };
+            }
+        }
+
+        await updateProducto.save();
+        console.log(updateProducto);
+        
+        return res.status(200).json(updateProducto);
+
+    } catch (error) {
+
+        console.log(error.message);
+    }
+};
+
+
+const deleteProductos = async (req, res) => {
+    try {
+        const productRemoved = await Producto.findByIdAndDelete(req.params.id);
+
+        if (!productRemoved) {
+        //const error = new Error("Token no valido");
+            return res.sendStatus(404);
+        } else {
+            if (productRemoved.image.public_id) {
+                await deleteImage(productRemoved.image.public_id);
+            }
+            return res.status(200).json({
+                msg: "Producto Eliminado exitosamente"
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+const getProducto = async (req, res) => {
+    try {
+        const OneProduct = await Producto.findById(req.params.id);
+        if (!OneProduct) {
+            return res.status(404).json({
+            msg: "No se encontro el producto"
+            });
+        } else {
+            return res.json(OneProduct);
+        }
+    } catch (error) {
+        return res.status(500).json({ msg: "No se encontro el producto" });
+    }
+};
+
+
+
+
+
+export {
+    prueba,
+    getProductos,
+    createProductos,
+    updateProductos,
+    deleteProductos,
+    getProducto
+};
---
+++ index.js
@@ -0,0 +1,65 @@
+// https://expressjs.com/es/
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import fileUpload from "express-fileupload";
+
+import conectarDB from './conf/db.js';
+import usuarioRoutes from './routes/usuarioRoutes.js';
+import ventaRoutes from './routes/ventaRoutes.js';
+import productoRoutes from './routes/productoRoutes.js';
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+
+// Se le agrega toda la funcionalidad del servidor de express
+const app = express();
+
+app.use(express.json());//recibimos la info en formato de json
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: './files'
+
+
+}));
+
+
+conectarDB();
+
+// middlewares
+// Se utiliza para realizar la comunicacion entre el servidor del frontend y el backend
+
+
+// const dominiosPermitidos = [process.env.FRONTEND_URL];
+
+// const corsOptions = {
+//     origin: function(origin, callback){
+//         if(dominiosPermitidos.indexOf(origin) !== -1){
+
+// // El origen del Request esta permitido
+//             callback(null, true);
+//         }else{
+//             callback(new Error('No permitido por CORS'));
+//         }
+//     }
+// };
+
+// app.use(cors(corsOptions));
+app.use(cors());
+
+//gestion modelo de usuarios
+app.use('/api/usuarios', usuarioRoutes);
+
+//gestion modelo de  productos
+app.use('/api/productos', productoRoutes);
+
+//gestion modelo de ventas
+app.use('/api/ventas', ventaRoutes);
+
+app.listen(PORT, () => {
+
+    console.log(`Servidor funcionando en el puerto ${PORT} `);
+
+});
